c17509f9e3f0e2afcb8819bb9a5c20b21e9f0704
Ömer Elikalfa <94922496+redensy@users.noreply.github.com>
2024-01-19 21:59:18 +0300

    added a test page for some copilot suggestions

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function toAboutPage(){
     window.location.replace("https://redensy.github.io/redensy/about/");
 }
+
+function toTestPage(){
+    window.location.replace("https://redensy.github.io/redensy/test/");
+}
